45e9ad2f8d7d5661b014be12996e6018a5cfa808
Aditya <shanbhag.ad@northeastern.edu>
2025-09-22 02:33:58 -0400

    Updating file structure

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,4 @@
-# server/app/main.py  (run this with uvicorn)
+# backend/app/main.py  (run this with uvicorn)
 import re
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
