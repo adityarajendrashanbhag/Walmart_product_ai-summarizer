25689669a5025ec8d9bbe2b1a105ecfeec0c68cc
Aditya <shanbhag.ad@northeastern.edu>
2025-09-23 03:30:39 -0400

    Restoring Original

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -46,7 +46,7 @@ def typing_effect(text, placeholder):
 
 
 st.set_page_config(page_title="Walmart Review Analyzer", page_icon="🛒", layout="centered")
-st.title("Walmart Product Reviews AI Summarizer Aditya")
+st.title("Walmart Product Reviews AI Summarizer")
 
 with st.form("summarize_form"):
     url = st.text_input("Enter Walmart product URL")
